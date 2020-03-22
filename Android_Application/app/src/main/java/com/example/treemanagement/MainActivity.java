ebd5e6727fa16488fe29572bcfdc777feeff3ccb
psy1064 <psy1064@naver.com>
2020-03-22 23:57:38 +0900

    php에서 Json 파싱

---
+++ Android_Application/app/src/main/java/com/example/treemanagement/MainActivity.java
@@ -0,0 +1,138 @@
+package com.example.treemanagement;
+
+import android.content.pm.ActivityInfo;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView textView;
+    private String jsonString;
+    ArrayList<Tree> treeArrayList;      // 나무정보들을 저장할 ArrayList
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView);
+
+        JsonParse jsonParse = new JsonParse();      // AsyncTask 생성
+        jsonParse.execute("http://121.153.150.157:81/connect.php");     // AsyncTask 실행
+    }
+
+    public class JsonParse extends AsyncTask<String, Void, String> {
+        String TAG = "JsonParseTest";
+        @Override
+        protected String doInBackground(String... strings) {    // execute의 매개변수를 받아와서 사용
+            String url = strings[0];
+            try {
+                URL serverURL = new URL(url);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) serverURL.openConnection();
+
+                httpURLConnection.setReadTimeout(5000);
+                httpURLConnection.setConnectTimeout(5000);
+                httpURLConnection.connect();
+
+                int responseStatusCode = httpURLConnection.getResponseCode();
+
+                InputStream inputStream;
+                if(responseStatusCode == HttpURLConnection.HTTP_OK) {
+                    inputStream = httpURLConnection.getInputStream();
+                }
+                else{
+                    inputStream = httpURLConnection.getErrorStream();
+                }
+
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                StringBuilder sb = new StringBuilder();
+                String line;
+
+                while((line = bufferedReader.readLine()) != null){
+                    sb.append(line);
+                }
+
+                bufferedReader.close();
+                Log.d(TAG, sb.toString().trim());
+
+                return sb.toString().trim();        // 받아온 JSON의 공백을 제거
+            } catch (Exception e) {
+                Log.d(TAG, "InsertData: Error ", e);
+                String errorString = e.toString();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String fromdoInBackgroundString) { // doInBackgroundString에서 return한 값을 받음
+            super.onPostExecute(fromdoInBackgroundString);
+
+            if(fromdoInBackgroundString == null)
+                textView.setText("error");
+            else {
+                jsonString = fromdoInBackgroundString;
+                treeArrayList = doParse();
+                Log.d(TAG,treeArrayList.get(0).getName());
+                textView.setText(treeArrayList.get(0).getName());
+            }
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
+        }
+
+
+        private ArrayList<Tree> doParse() {
+            ArrayList<Tree> tmpTreeArray = new ArrayList<Tree>();
+            try {
+                JSONObject jsonObject = new JSONObject(jsonString);
+                JSONArray jsonArray = jsonObject.getJSONArray("Tree");
+
+                for(int i=0;i<jsonArray.length();i++) {
+                    Tree tmpTree = new Tree();
+                    JSONObject item = jsonArray.getJSONObject(i);
+                    tmpTree.setName(item.getString("이름"));
+                    tmpTree.setCategory(item.getString("카테고리"));
+                    tmpTree.setKeyword(item.getString("키워드"));
+                    tmpTree.setNumber(item.getInt("보유수량"));
+                    tmpTree.setPrice(item.getInt("주당가격"));
+                    tmpTree.setLocation(item.getString("위치"));
+                    tmpTree.setEct(item.getString("특이사항"));
+                    tmpTree.setTreeHeight(item.getString("수고"));
+                    tmpTree.setRootCollar(item.getString("근원직경"));
+                    tmpTree.setBreastHeight(item.getString("흉고직경"));
+                    tmpTree.setWidthCrown(item.getString("수관폭"));
+                    tmpTree.setLength(item.getString("수관길이"));
+                    tmpTree.setCrownHeight(item.getString("지하고"));
+                    tmpTree.setManner(item.getString("육종방법"));
+                    tmpTree.setDateTime(item.getString("입력시간"));
+
+                    tmpTreeArray.add(tmpTree);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return tmpTreeArray;
+        } // JSON을 가공하여 ArrayList에 넣음
+    }
+}
---
+++ Android_Application/app/src/main/java/com/example/treemanagement/Tree.java
@@ -0,0 +1,141 @@
+package com.example.treemanagement;
+
+public class Tree {
+    String name;            // 이름
+    String category;        // 카테고리
+    String keyword;         // 키워드
+    int number;             // 보유수량
+    int price;              // 주당가격
+    String location;        // 위치
+    String ect;             // 특이사항
+    String treeHeight;      // 수고
+    String rootCollar;      // 근원직경
+    String breastHeight;    // 흉고직경
+    String widthCrown;      // 수관폭
+    String length;          // 수관길이
+    String crownHeight;     // 지하고
+    String manner;          // 육종방법
+    String dateTime;        // 입력시간
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getKeyword() {
+        return keyword;
+    }
+
+    public void setKeyword(String keyword) {
+        this.keyword = keyword;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getEct() {
+        return ect;
+    }
+
+    public void setEct(String ect) {
+        this.ect = ect;
+    }
+
+    public String getTreeHeight() {
+        return treeHeight;
+    }
+
+    public void setTreeHeight(String treeHeight) {
+        this.treeHeight = treeHeight;
+    }
+
+    public String getRootCollar() {
+        return rootCollar;
+    }
+
+    public void setRootCollar(String rootCollar) {
+        this.rootCollar = rootCollar;
+    }
+
+    public String getBreastHeight() {
+        return breastHeight;
+    }
+
+    public void setBreastHeight(String breastHeight) {
+        this.breastHeight = breastHeight;
+    }
+
+    public String getWidthCrown() {
+        return widthCrown;
+    }
+
+    public void setWidthCrown(String widthCrown) {
+        this.widthCrown = widthCrown;
+    }
+
+    public String getLength() {
+        return length;
+    }
+
+    public void setLength(String length) {
+        this.length = length;
+    }
+
+    public String getCrownHeight() {
+        return crownHeight;
+    }
+
+    public void setCrownHeight(String crownHeight) {
+        this.crownHeight = crownHeight;
+    }
+
+    public String getManner() {
+        return manner;
+    }
+
+    public void setManner(String manner) {
+        this.manner = manner;
+    }
+
+    public String getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(String dateTime) {
+        this.dateTime = dateTime;
+    }
+
+
+}
